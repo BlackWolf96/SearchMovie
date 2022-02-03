39a492e6c09a5045d277a9dae3bbf1eb24ccd1f9
BlackWolf96 <oskar.blackwolf@gmail.com>
2022-02-03 21:44:00 +0100

    Update

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,13 +1,49 @@
 <template>
-  
+  <header>
+    <nav>
+      <nuxt-link to="/">
+        Home
+      </nuxt-link>
+      <nuxt-link to="/search">
+        search
+      </nuxt-link>
+      <nuxt-link to="/about">
+        about
+      </nuxt-link>
+      <nuxt-link to="/contact">
+        contact
+      </nuxt-link>
+    </nav>
+  </header>
 </template>
 
 <script>
 export default {
-
+  name: 'NavbarComponent'
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+header{
+  width:100%;
+  height:10em;
 
+
+  @media only screen and (max-width:600px){
+    nav{
+      height:10em;
+      display:flex;
+      flex-direction: column;
+      a{ text-decoration: none; color:white; }
+    }
+  }
+  @media only screen and (min-width:600px){
+    nav{
+      height:10em;
+      display:flex;
+      flex-direction: row;
+      a{ text-decoration: none; color:white; }
+    }
+  }
+}
 </style>
