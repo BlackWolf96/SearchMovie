3e382c06e67f8ac5cccdfa8f92971ad2e31bc452
BlackWolf96 <oskar.blackwolf@gmail.com>
2022-02-04 11:45:49 +0100

    Update

--- components/Search.vue
+++ components/Search.vue
@@ -4,15 +4,22 @@
       <input type="search" name="" id="">
       <button>Search</button>
     </div>
-    <div class="results">
-
+    <div class="results spacer" v-if="results">
+      <div v-for="result in results" :key="result.id">
+        <div>{{ result. img }}</div>
+        <div>{{ result. title }}</div>
+        <div>{{ result. description }}</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'SearchComponent'
+  name: 'SearchComponent',
+  results: [
+    { img: '', title: '', description: '' }
+  ]
 }
 </script>
 
@@ -30,24 +37,27 @@ $btn-hover: rgba(0,0,0, 0.7);
       height:40px;
       border-style:none;
       border-bottom: 1px solid black;
-    }
-    input:focus{
+      &:hover{
         outline: none;
       }
+    }
     button{
       width:100px;
       height:40px;
       background-color: black;
       border-color:black;
       color:white;
-      :hover{
+      &:hover{
         background-color: $btn-hover;
       }
     }
   }
   .results{
-    width:100%;
-    min-height:200px
+    width:90%;
+    background-color: yellow;
+    min-height:200px;
+    border:1px solid black;
+    border-radius: 5px;
   }
 }
 </style>
