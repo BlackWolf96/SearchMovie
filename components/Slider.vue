8c7842f7d332f593421dea0fef16ea1fc243a165
BlackWolf96 <oskar.blackwolf@gmail.com>
2022-02-04 13:44:12 +0100

    Update

--- components/Slider.vue
+++ components/Slider.vue
@@ -8,15 +8,15 @@
     </VueSlickCarousel>
   </div>
 </template>
- 
+
 <script>
 import VueSlickCarousel from 'vue-slick-carousel'
 import 'vue-slick-carousel/dist/vue-slick-carousel.css'
 // optional style for arrows & dots
 import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
- 
+
 export default {
   name: 'MyComponent',
-  components: { VueSlickCarousel },
+  components: { VueSlickCarousel }
 }
-</script> 
+</script>
