0c204f12e4ace0ad4a20b7cad43f78a74e38034e
BlackWolf96 <oskar.blackwolf@gmail.com>
2022-02-04 21:28:35 +0100

    Update

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Navbar />
+    <Search />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage'
+}
+</script>
--- pages/search.vue
+++ pages/search.vue
@@ -7,6 +7,6 @@
 
 <script>
 export default {
-  name: 'searchpage'
+  name: 'SearchPaege'
 }
 </script>
