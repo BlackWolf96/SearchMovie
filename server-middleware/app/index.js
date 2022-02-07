8a9629d095b9da28da2cea3c69c80ddb7e431a6e
BlackWolf96 <oskar.blackwolf@gmail.com>
2022-02-07 22:34:58 +0100

    Update

--- server-middleware/app/index.js
+++ server-middleware/app/index.js
@@ -7,27 +7,52 @@ const connection = mysql.createConnection({
   password: process.env.password,
   database: process.env.database
 })
-
 const myapp = express()
 const port = 3001
+
 myapp.use(express.json());
 myapp.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, CREATE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next()
 })
-connection.connect();
 
+connection.connect();
 connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results[0].solution);
 });
 
+// TEST TABLE
+const test_data = Array(
+  { 'actress': 'Hugh Jackman', 'movie': 'Van Helsing', 'poster': 'Not yet' },
+  { 'actress': 'Kate Beckinsale', 'movie': 'Van Helsing', 'poster': 'Not yet' },
+  { 'actress': 'Sean Connery', 'movie': 'Liga niezwykłych dżentelmenów', 'poster': 'Not yet' },
+  { 'actress': 'Stuart Townsend', 'movie': 'Liga niezwykłych dżentelmenów', 'poster': 'Not yet' }
+)
+
+connection.query(`CREATE TABLE IF NOT EXISTS test_table (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    actress_name VARCHAR( 255 ) NOT NULL, 
+    movie_name VARCHAR( 120 ) NOT NULL, 
+    movie_poster VARCHAR( 200 ) NOT NULL 
+  ) engine=INNODB
+  `);
+connection.query('SELECT * from test', (error, results, fields) => {
+  /*
+  if(results.length < 1){
+    test_data.forEach(item => {
+      connection.query(`INSERT INTO test (actress_name,movie_name,movie_poster) VALUES(${item.actress}, ${item.movie}, ${item.poster})`)
+    })
+  }
+  */
+})
+// END TEST TABLE
 myapp.get('/', (req, res) => {
   connection.query('SELECT * from movie', (error, results, fields) => {
-    if (error) throw error;
+    console.log( results )
     res.send(JSON.stringify(results))
   });
 
