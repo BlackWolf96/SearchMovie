0d0fa2765e2560aec5426163f8e0a06a132cf938
BlackWolf96 <oskar.blackwolf@gmail.com>
2022-02-08 18:39:58 +0100

    Update

--- server-middleware/app/index.js
+++ server-middleware/app/index.js
@@ -21,7 +21,8 @@ myapp.use((req, res, next) => {
   res.setHeader('Content-Security-Policy', "default-src 'self'")
   res.setHeader('Strict-Transport-Security', 'max-age=63072000; includeSubDomains; preload')
   res.setHeader('X-XSS-Protection', '1; mode=block')
-  res.setHeader(' X-Content-Type-Options', 'nosniff')
+  res.setHeader('X-Content-Type-Options', 'nosniff')
+  res.setHeader('X-Frame-Options', 'DENY')
   next()
 })
 
